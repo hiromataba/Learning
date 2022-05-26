e48a3a95d3b5cfd74be40f0f7c969dfec0bbb844
Hiro Mataba <aganzemataba@gmail.com>
2022-05-26 14:45:12 +0200

    Add student data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Student } from './models/Student';
+
+import studentsData from './students.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'personal';
+  students: Student[] = studentsData;
 }
--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../../models/Student';
+import studentsData from '../../students.json';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.scss']
+  styleUrls: ['./students.component.scss'],
 })
 export class StudentsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  students: Student[] = studentsData;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
